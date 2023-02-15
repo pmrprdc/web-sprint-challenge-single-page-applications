53a6a801323e1fc8fe31e81fc5d4906bcf3b4e8e
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-15 06:10:53 -0500

    have multiple topping checkboxes rendered

--- src/components/Order.js
+++ src/components/Order.js
@@ -57,6 +57,61 @@ const onChange = evt =>{
                                     Pepperoni
                                     <input type="checkbox"/>
                                 </label>
+                                <label>
+                                    Sausage
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Canadian Bacon
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Spicy Italian Sausage
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Grilled Chicken
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Onions
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Greem Pepper
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Diced Tomatos
+                                    <input type="checkbox"/>
+                                </label>
+                               
+                                <br></br>
+                                <label>
+                                    Black Olives
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Roasted Garlic
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Artichoke Hearts
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Three Cheese
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Pineapple
+                                    <input type="checkbox"/>
+                                </label>
+                                <label>
+                                    Extra Cheese
+                                    <input type="checkbox"/>
+                                </label>
+                              
                             <button>Submit</button>
                         </label>
 
