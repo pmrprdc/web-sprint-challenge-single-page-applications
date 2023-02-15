a98c609d4afa61c19eac4944041db0dcad0e390a
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-15 06:21:03 -0500

    values added to pizza sauce radio selection

--- src/components/Order.js
+++ src/components/Order.js
@@ -36,19 +36,20 @@ const onChange = evt =>{
                             <br></br>
                             <label>
                                 Original Red
-                                <input type="radio" name="sauce" />
+                                <input type="radio" name="sauce" value="original-red" />
                             </label>
                             <label>
                                 Garlic Ranch
-                                <input type="radio" name="sauce" />
+                                <input type="radio" name="sauce" value="garlic-ranch" />
                             </label>
                             <label>
                                 BBQ Sauce
-                                <input type="radio" name="sauce" />
+                                <input type="radio" name="sauce" value="bbq-sauce" />
                             </label>
                             <label>
                                 Spinach Alfredo
-                                <input type="radio" name="sauce" />
+                                <input type="radio" name="sauce"  value="spinach-alfredo"
+                                />
                             </label>
 
                             <h2>Let's add toppings!</h2>
