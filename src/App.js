45ac9aaa21c013cd9128c25fc081fa948aad2603
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-15 05:28:33 -0500

    Added form component and it is rendering in the root route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Route, Link} from "react-router-dom"
 import Nav from "./components/Nav"
+import Home from "./components/Home"
 
 const App = () => {
   return (
     <>
       <Nav />
       <Route path="/">
-
+        <Home />
       </Route>
       <Route path="order">
 
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function Home(props){
+
+
+    return (<h1>Home Page</h1>)
+}
